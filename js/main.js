07a12b713a6bf82401987cb38e8d5843fad0122a
nicolass.vcv <nicolass.vescovo@gmail.com>
2024-03-04 21:03:35 -0300

    Pre entrega1

--- js/main.js
+++ js/main.js
@@ -4,5 +4,5 @@ let contrasena2 = prompt("Ingresa nuevamente tu contraseña:");
 if (contrasena==contrasena2){
     console.log ("Contraseña correcta. Bienvenido " + usuario);   
 }else{
-    prompt("Contraseña incorrecta. Ingresa tu contraseña nuevamente");
+    Alert("Contraseña incorrecta, acceso denegado.");
 }
