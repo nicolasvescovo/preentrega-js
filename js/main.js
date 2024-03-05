a0724fcb045628bf61ceaf756ff6dc3ab619da16
nicolass.vcv <nicolass.vescovo@gmail.com>
2024-03-04 21:35:09 -0300

    Primer pre-entrega

--- js/main.js
+++ js/main.js
@@ -6,3 +6,24 @@ if (contrasena==contrasena2){
 }else{
     Alert("Contraseña incorrecta, acceso denegado.");
 }
+
+let dia = Number (prompt("Indique con número del 1 al 5 el día de la semana para ver la actividad que tiene pendiente."))
+switch (dia){
+    case 1:
+        console.log (usuario +" hoy tiene reunión a las 15hs")
+        break
+    case 2:
+        console.log (usuario +" hoy tiene turno en el médico.")
+        break
+    case 3:
+        console.log (usuario +" hoy tiene turno con el psicólogo a las 19hs.")
+        break
+    case 4:
+        console.log(usuario +" hoy tiene el día libre.")
+        break
+    case 5:
+        console.log(usuario +" hoy tiene partido de futbol a las 20hs y después asado con amigos")
+        break
+    default:
+        console.log ("Ingreso un dato incorrecto.")
+}
